8b9619bad31b57e503927d63bd4edd4cd16a63e0
ChickenInKFCgit <linktriforce2611@gmail.com>
2026-09-15 20:53:47 +0200

    v0.5

--- lambeauxmaker.py
+++ lambeauxmaker.py
@@ -3,12 +3,12 @@
 de base le placeholder c'est who is he??
 """
 import os 
+from datetime import date
 from PIL import Image, ImageDraw, ImageFont
 
 PATH_MJS = "images/photosmjs/"
-            
-FONT_MAIN = ImageFont.truetype("segoeui.ttf", size=36)
-FONT_SUB = ImageFont.truetype("segoeui.ttf", size=20)
+FONT_SUB = ImageFont.truetype("segoeui.ttf", size=100)      
+FONT_MVP = ImageFont.truetype("fonts/Anton-Regular.ttf", size=240) 
 SUBTEXT_COLOR = (100, 200, 100, 255)
 
 def load_img(path:str)-> Image:
@@ -17,25 +17,34 @@ def load_img(path:str)-> Image:
 class LambeauxMaker:
     def __init__(self, victoire:bool, scoreEq1:int, scoreEq2:int, nomMVP:str):  
         imgMVP = self.load_MVP(nomMVP)
+        self.mvp = nomMVP
 
-        WorLText = "VICTOIRE" if victoire else "DÉFAITE"
+        winlooseIMG = self.load_MainText(victoire)
         textcolor = (0, 255, 0, 255) if victoire else (255, 0, 0, 255)
         ScoreText = f"\ {scoreEq1} - {scoreEq2} /"
         background = self.load_Background()
 
-        self.merge(background, imgMVP, WorLText ,ScoreText, textcolor)
+        self.merge(background, imgMVP, winlooseIMG ,ScoreText, textcolor)
 
-    def merge(self, backgroundIMG:Image, profileIMG:Image, mainText:str, subText:str, text_color:tuple):
-        backgroundIMG.paste(profileIMG, (0,0) , profileIMG) 
+    def merge(self, backgroundIMG:Image, profileIMG:Image, mainTextIMG:Image, subText:str, text_color:tuple):
+        pos_x = backgroundIMG.size[0]//2 - profileIMG.size[0]//2
+        pos_y = backgroundIMG.size[1]//2 - profileIMG.size[1]//2
+        backgroundIMG.paste(profileIMG, (pos_x,pos_y) , profileIMG) 
 
-        draw = ImageDraw.Draw(backgroundIMG)  
+        pos_x = backgroundIMG.size[0]//2 - mainTextIMG.size[0]//2
+        pos_y = int(backgroundIMG.size[1]*1.05)- mainTextIMG.size[1]
+        backgroundIMG.paste(mainTextIMG, (pos_x,pos_y) , mainTextIMG)  
+        draw = ImageDraw.Draw(backgroundIMG)
 
-        draw.text( (50,50) , mainText, fill=text_color, font=FONT_MAIN) 
-        draw.text( (100,100) , subText, fill=SUBTEXT_COLOR, font=FONT_SUB)   
+        pos_y //= 4
+        draw.text( (pos_x,pos_y) , subText, fill=SUBTEXT_COLOR, font=FONT_SUB)   
 
         #sauvegarde
-        backgroundIMG.convert("RGB").save("creationsAJDDATEJSP.jpg") 
+        backgroundIMG.convert("RGB").save(f"images/creations/{date.today()}=={self.mvp}==.jpg") 
     
+    def load_MainText(self, win:bool):
+        path = "images/win.png" if win else "images/loose.png"
+        return load_img(path)
 
     def load_MVP(self, mj:str)-> Image:
         chemin = f"{PATH_MJS}{mj}.png"
